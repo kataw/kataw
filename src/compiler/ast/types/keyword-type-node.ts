53a483fcf21b9b0acdd8d127ca951b6f0e0eda24
Kenny F <kfrachelle123@gmail.com>
2021-01-28 10:22:17 +0800

    docs(all): updated AST specs

---
+++ src/compiler/ast/types/keyword-type-node.ts
@@ -0,0 +1,34 @@
+import { Node, NodeFlags, NodeKind, TransformFlags } from '../node';
+
+/**
+ * KeywordTypeNode
+ *
+ *  NodeKind.VoidKeyword
+ *  NodeKind.NullKeyword
+ *  NodeKind.AnyKeyword
+ *  NodeKind.UnknownKeyword
+ *  NodeKind.StringKeyword
+ *  NodeKind.NumberKeyword
+ *  NodeKind.BigIntKeyword
+ *  NodeKind.SymbolKeyword
+ *  NodeKind.BooleanKeyword
+ *  NodeKind.UndefinedKeyword
+ *  NodeKind.NeverKeyword
+ *  NodeKind.ObjectKeyword
+ *
+ */
+
+export type KeywordTypeNode = Node;
+
+export function createKeywordTypeNode(kind: NodeKind, start: number, end: number): KeywordTypeNode {
+  return {
+    kind,
+    flags: NodeFlags.None,
+    intersects: false,
+    transformFlags: TransformFlags.TypeScript,
+    parent: null,
+    emitNode: null,
+    start,
+    end
+  };
+}
