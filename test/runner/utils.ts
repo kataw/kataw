f23544c2201fbb24c647d2bf35689c52bbb3ac91
唯刃 <weiren@dian.so>
2021-03-16 13:37:04 +0800

    fix: edge case in deepEqual

--- test/runner/utils.ts
+++ test/runner/utils.ts
@@ -101,8 +101,9 @@ export function deepEqual(obj1: any, obj2: any): boolean {
 
   if (type1 === '[object Object]') {
     const keys1 = Object.keys(obj1);
+    const keys2 = Object.keys(obj2);
 
-    return keys1.every((k) => deepEqual(obj1[k], obj2[k]));
+    return keys1.every(k => keys2.includes(k)) && keys1.every((k) => deepEqual(obj1[k], obj2[k]));
   }
 
   return obj1 === obj2;
