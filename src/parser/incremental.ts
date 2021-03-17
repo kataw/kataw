6ad95158a19f5f3d4cfdf608e232cfb67c869fb6
Kenny F <kfrachelle123@gmail.com>
2021-03-17 09:04:29 +0800

    chore: removed unused file

--- src/parser/incremental.ts
+++
@@ -1,6 +0,0 @@
-import { ParserState } from '../types';
-import { Context } from './common';
-
-export function getCurrentNode(parser: ParserState, context: Context, cb: any): any {
-  return cb(parser, context);
-}
