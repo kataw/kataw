4aab1fa5cf074e254f21f0ca979f64f32cf9ea69
Kenny F <kfrachelle123@gmail.com>
2021-03-21 10:35:54 +0800

    chore: removed types from func and class decl in TS transformer

--- src/transform/transformers/typescript.ts
+++ src/transform/transformers/typescript.ts
@@ -3,6 +3,8 @@ import { Node, NodeKind, NodeFlags } from '../../ast/node';
 import { createVariableDeclarationList, VariableDeclarationList } from '../../ast/statements/variable-declarationList';
 import { createVariableDeclaration, VariableDeclaration } from '../../ast/statements/variable-declaration';
 import { createVariableStatement, VariableStatement } from '../../ast/statements/variable-stmt';
+import { updateFunctionDeclaration, FunctionDeclaration } from '../../ast/statements/function-declaration';
+import { updateClassDeclaration, ClassDeclaration } from '../../ast/statements/class-declaration';
 
 export function transformTpescript(context: any): any {
   return transformSourceFile;
@@ -16,7 +18,6 @@ export function transformTpescript(context: any): any {
   }
 
   function visitTypeScript(node: any): any {
-
     switch (node.kind) {
       case NodeKind.StringKeyword:
       case NodeKind.BindingElement:
@@ -51,26 +52,93 @@ export function transformTpescript(context: any): any {
       case NodeKind.Decorator:
       case NodeKind.TypeAliasDeclaration:
       case NodeKind.NamespaceExportDeclaration:
+      case NodeKind.InterfaceDeclaration:
         return null;
+      case NodeKind.TypeAssertionExpression:
+      case NodeKind.AsExpression:
+        // TypeScript type assertions are removed, but their subtrees are preserved.
+        return visitAssertionExpression(node);
+      case NodeKind.VariableDeclaration:
+        return visitVariableDeclaration(node);
+      case NodeKind.ClassDeclaration:
+        return visitClassDeclaration(node);
+      case NodeKind.AsyncFunctionDeclaration:
+        return visitAsyncFuncionDeclaration(node);
+      case NodeKind.FunctionDeclaration:
+        return visitFuncionDeclaration(node);
       case NodeKind.EnumDeclaration:
         return visitEnumDeclaration(node);
     }
-    //console.log(node)
+
     return visitEachChild(node, visitor, context);
+  }
+
+  function visitAssertionExpression(node: any): any {
+    return node;
+  }
+
+  function visitAsyncFuncionDeclaration(node: any): any {
+    node.flags = (node.flags | NodeFlags.Declared) ^ NodeFlags.Declared;
+    return updateFunctionDeclaration(
+      node,
+      /* isGenerator */ node.isGenerator,
+      /* isAsync */ 1,
+      /* name */ node.name,
+      /* formalParameters */ node.formalParameters,
+      /* contents */ node.contents,
+      /* typeParameters */ null,
+      /* type */ null
+    );
+  }
+
+  function visitFuncionDeclaration(node: any): any {
+    node.flags = (node.flags | NodeFlags.Declared) ^ NodeFlags.Declared;
+    return updateFunctionDeclaration(
+      node,
+      /* isGenerator */ node.isGenerator,
+      /* isAsync */ 0,
+      /* name */ node.name,
+      /* formalParameters */ node.formalParameters,
+      /* contents */ node.contents,
+      /* typeParameters */ null,
+      /* type */ null
+    );
+  }
+
+  function visitClassDeclaration(node: any): any {
+    node.flags = (node.flags | NodeFlags.Declared | NodeFlags.Abstract) ^ (NodeFlags.Declared | NodeFlags.Abstract);
+    return updateClassDeclaration(
+      node,
+      /* name */ node.name,
+      /* typeParameters */ null,
+      /* classHeritage */ node.classHeritage,
+      /* implementClauses */ null,
+      /* members */ node.members,
+      /* decorators */ null,
+      /* isAbstract */ false
+    );
+  }
+
+  function visitVariableDeclaration(node: any): any {
+    node.exclamation = null;
+    node.type = null;
+    return node;
   }
 
   function visitEnumDeclaration(node: any): any {
     const statements: any[] = [];
-    console.log(node.members.enumMembersList)
+    console.log(node.members.enumMembersList);
 
     const varAdded = createVariableStatement(
       createVariableDeclarationList(
-        [createVariableDeclaration(node.members.enumMembersList as any, false, null, null, NodeFlags.None, -1, -1)]
-        , NodeFlags.None, -1, -2),
+        [createVariableDeclaration(node.members.enumMembersList as any, false, null, null, NodeFlags.None, -1, -1)],
+        NodeFlags.None,
+        -1,
+        -2
+      ),
       NodeFlags.None,
       -1,
       -1
     );
-
   }
 }
