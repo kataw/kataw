b97c259f8d64c207246ee66b49f94d80aa1cf73f
Kenny F <kfrachelle123@gmail.com>
2021-03-21 13:30:32 +0800

    chore: WIP: TS to JS transformer

--- src/transform/transformers/typescript.ts
+++ src/transform/transformers/typescript.ts
@@ -1,10 +1,17 @@
-import { visitEachChild } from '../../visitor';
+import { visitEachChild, visitNode } from '../../visitor';
 import { Node, NodeKind, NodeFlags } from '../../ast/node';
 import { createVariableDeclarationList, VariableDeclarationList } from '../../ast/statements/variable-declarationList';
 import { createVariableDeclaration, VariableDeclaration } from '../../ast/statements/variable-declaration';
 import { createVariableStatement, VariableStatement } from '../../ast/statements/variable-stmt';
 import { updateFunctionDeclaration, FunctionDeclaration } from '../../ast/statements/function-declaration';
 import { updateClassDeclaration, ClassDeclaration } from '../../ast/statements/class-declaration';
+import { createBinaryExpression } from '../../ast/expressions/binary-expr';
+import { createAssignmentExpression } from '../../ast/expressions/assignment-expr';
+import { createObjectLiteral } from '../../ast/expressions/object-literal';
+import { createPropertyDefinitionList } from '../../ast/expressions/property-definition-list';
+import { createFunctionExpression } from '../../ast/expressions/function-expr';
+import { createCallExpression } from '../../ast/expressions/call-expr';
+import { createExpressionStatement } from '../../ast/statements/expr-stmt';
 
 export function transformTpescript(context: any): any {
   return transformSourceFile;
@@ -66,6 +73,10 @@ export function transformTpescript(context: any): any {
         return visitAsyncFuncionDeclaration(node);
       case NodeKind.FunctionDeclaration:
         return visitFuncionDeclaration(node);
+      case NodeKind.AsyncFunctionExpression:
+        return visitAsyncFuncionExpression(node);
+      case NodeKind.FunctionExpression:
+        return visitFuncionFuncionExpression(node);
       case NodeKind.EnumDeclaration:
         return visitEnumDeclaration(node);
     }
@@ -78,8 +89,7 @@ export function transformTpescript(context: any): any {
   }
 
   function visitAsyncFuncionDeclaration(node: any): any {
-    node.flags = (node.flags | NodeFlags.Declared) ^ NodeFlags.Declared;
-    return updateFunctionDeclaration(
+    const updated = updateFunctionDeclaration(
       node,
       /* isGenerator */ node.isGenerator,
       /* isAsync */ 1,
@@ -89,20 +99,56 @@ export function transformTpescript(context: any): any {
       /* typeParameters */ null,
       /* type */ null
     );
+    updated.flags = (updated.flags | NodeFlags.Declared) ^ NodeFlags.Declared;
+    return updated;
   }
 
   function visitFuncionDeclaration(node: any): any {
     node.flags = (node.flags | NodeFlags.Declared) ^ NodeFlags.Declared;
-    return updateFunctionDeclaration(
+    const updated = updateFunctionDeclaration(
       node,
       /* isGenerator */ node.isGenerator,
       /* isAsync */ 0,
       /* name */ node.name,
-      /* formalParameters */ node.formalParameters,
-      /* contents */ node.contents,
+      /* formalParameters */ visitNode(node.formalParameters, visitor),
+      /* contents */ visitNode(node.contents, visitor, context),
       /* typeParameters */ null,
       /* type */ null
     );
+    updated.flags = (updated.flags | NodeFlags.Declared) ^ NodeFlags.Declared;
+    return updated;
+  }
+
+  function visitAsyncFuncionExpression(node: any): any {
+    node.flags = (node.flags | NodeFlags.Declared) ^ NodeFlags.Declared;
+    const updated = updateFunctionDeclaration(
+      node,
+      /* isGenerator */ node.isGenerator,
+      /* isAsync */ 0,
+      /* name */ node.name,
+      /* formalParameters */ visitNode(node.formalParameters, visitor),
+      /* contents */ visitNode(node.contents, visitor, context),
+      /* typeParameters */ null,
+      /* type */ null
+    );
+    updated.flags = (updated.flags | NodeFlags.Declared) ^ NodeFlags.Declared;
+    return updated;
+  }
+
+  function visitFuncionFuncionExpression(node: any): any {
+    node.flags = (node.flags | NodeFlags.Declared) ^ NodeFlags.Declared;
+    const updated = updateFunctionDeclaration(
+      node,
+      /* isGenerator */ node.isGenerator,
+      /* isAsync */ 0,
+      /* name */ node.name,
+      /* formalParameters */ visitNode(node.formalParameters, visitor),
+      /* contents */ visitNode(node.contents, visitor, context),
+      /* typeParameters */ null,
+      /* type */ null
+    );
+    updated.flags = (updated.flags | NodeFlags.Declared) ^ NodeFlags.Declared;
+    return updated;
   }
 
   function visitClassDeclaration(node: any): any {
@@ -127,6 +173,12 @@ export function transformTpescript(context: any): any {
 
   function visitEnumDeclaration(node: any): any {
     const statements: any[] = [];
+    if (node.isConst) {
+      // TODO
+    }
+
+    node.flags = (node.flags | NodeFlags.Declared) ^ NodeFlags.Declared;
+
     console.log(node.members.enumMembersList);
 
     const varAdded = createVariableStatement(
@@ -136,9 +188,44 @@ export function transformTpescript(context: any): any {
         -1,
         -2
       ),
-      NodeFlags.None,
+      NodeFlags.Let,
       -1,
       -1
     );
+
+    const exportName = node.name;
+
+    //  x || (x = {})
+    //  exports.x || (exports.x = {})
+
+    let moduleArg = createBinaryExpression(
+      exportName,
+      '||',
+      createAssignmentExpression(
+        exportName,
+        '=',
+        createObjectLiteral(
+          createPropertyDefinitionList(
+            [],
+            /* trailingComma */ false,
+            /* multiline */ false,
+            /* NodeFlags */ NodeFlags.None,
+            /* start */ -1,
+            /* end */ -1
+          ),
+          /* NodeFlags */ NodeFlags.None,
+          /* start */ -1,
+          /* end */ -1
+        ),
+        /* NodeFlags */ NodeFlags.None,
+        /* start */ -1,
+        /* end */ -1
+      ),
+      /* NodeFlags */ NodeFlags.None,
+      /* start */ -1,
+      /* end */ -1
+    );
   }
+
+  // TODO
 }
