869669c62d10c0473469766aea846080d820d738
Kenny F <kfrachelle123@gmail.com>
2021-07-26 17:40:32 +0800

    chore: add reporters

---
+++ src/reporter/aladdin.ts
@@ -0,0 +1,21 @@
+import { DiagnosticSource, DiagnosticKind,  } from '../diagnostic/diagnostic';
+import { ColorCodes, diagnosticColor, getLineNumber, getLine, getLinePos } from './common';
+
+export function aladdin(diagnosticSource: DiagnosticSource, kind: DiagnosticKind,  message: string,  start: number, end: number, source: any) {
+  const highlightColor = diagnosticColor(kind);
+  const lineNumber = getLineNumber(source, start).toString();
+  const lnSpacing = ' '.repeat(lineNumber.length);
+  const line = getLine(source, start, end);
+  const relativePos = getLinePos(source, start, end);
+  const errLen = relativePos.end - relativePos.pos;
+  let errorMarker = highlightColor + '^' + ColorCodes.Reset.repeat(errLen);
+  // if the underline is 2 characters or less, add `--` to make it stand out more.
+  if (errLen <= 2) {
+    errorMarker +=  highlightColor + '--' + ColorCodes.Reset;
+  }
+  process.stdout.write(`${lnSpacing}${ColorCodes.Yellow + '-->' + ColorCodes.Reset } in ${ColorCodes.Cyan +'foo.js' + ColorCodes.Reset}\n\n`);
+  process.stdout.write(`${highlightColor + lineNumber + ColorCodes.Reset + ColorCodes.White + ' |' + ColorCodes.Reset} ${line}\n`);
+  process.stdout.write(`${lnSpacing} ${ColorCodes.White +'|'+ ColorCodes.Reset}`);
+  process.stdout.write(`${' '.repeat(relativePos.pos + 1)}${errorMarker}\n\n`);
+  process.stdout.write(`${highlightColor + message + ColorCodes.Reset}\n\n`);
+}
---
+++ src/reporter/common.ts
@@ -0,0 +1,59 @@
+import { DiagnosticSource, DiagnosticKind,  } from '../diagnostic/diagnostic';
+
+export const enum ColorCodes {
+  Gray = '\x1b[37;1m',
+  aWhite = '\u001b[32;1m',
+  White = '\x1b[37m',
+  Cyan = '\x1b[36m',
+  Blue = '\x1b[34m',
+  Blink = '\x1b[;5;1m',
+  Yellow = '\x1b[33m',
+  Red = '\x1b[31m',
+  Green = '\x1b[32m',
+  Reset = '\x1b[0m',
+}
+
+export function getLine(src: string, pos: number, end: number): string {
+  const lastLine = src.lastIndexOf('\n', pos) + 1; // cut off the \n
+  const nextLine = src.indexOf('\n', end);
+  return src.slice(lastLine, nextLine);
+}
+
+export function getLinePos(src: string, pos: number, end: number): any {
+  const lastLine = src.lastIndexOf('\n', pos) + 1; // cut off the \n
+  return {
+    pos: pos - lastLine,
+    end: end - lastLine,
+  };
+}
+
+
+export function getLineNumber(src: string, pos: number) {
+  let lineCount = 1;
+  let currentIdx = 0;
+  while (true) {
+    const next = src.indexOf('\n', currentIdx + 1);
+    if (next === -1) {
+      return lineCount + 1;
+    }
+    if (pos >= currentIdx && pos <= next) {
+      return lineCount;
+    }
+    lineCount++;
+    currentIdx = next;
+  }
+}
+
+export function diagnosticColor(kind: DiagnosticKind): any {
+  switch (kind) {
+    case DiagnosticKind.Hint:
+    case DiagnosticKind.Linter:
+      return ColorCodes.Green;
+    case DiagnosticKind.Error:
+      return ColorCodes.Red;
+    case DiagnosticKind.Message:
+      return ColorCodes.Red;
+    case DiagnosticKind.Warning:
+      return ColorCodes.Yellow;
+  }
+}
